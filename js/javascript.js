9b818821b1e06e34dfca7615abb41114328e6d1a
Bronson Carder <Bronsoncarder@gmail.com>
2021-03-10 20:37:59 -0600

    Initial Commit

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+// Scrollable Sticky Header
+window.onscroll = function() {
+  myFunction()
+};
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// Automatically stop when starting new track
+document.addEventListener('play', function(e) {
+  var audios = document.getElementsByTagName('audio');
+  for (var i = 0, len = audios.length; i < len; i++) {
+    if (audios[i] != e.target) {
+      audios[i].pause();
+    }
+  }
+}, true);
